f28a615e9527c23648a9b04957943cf7e406c559
mayonnaises <ernani.184404@gmail.com>
2021-07-01 20:44:49 +0900

    Update group chat

--- chatapp/admin.py
+++ chatapp/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import GroupRoom, OneonOneRoom, Message
+from .models import (
+    GroupMessage,
+    GroupRoom,
+    OneonOneRoom,
+    Message
+)
 
 
+admin.site.register(GroupMessage)
 admin.site.register(GroupRoom)
 admin.site.register(OneonOneRoom)
 admin.site.register(Message)
